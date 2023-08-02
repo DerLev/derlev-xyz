d2f96f08b0271c71e6297dd6b7c832e184583dcb
DerLev <levin.schroeren@gmail.com>
2023-08-02 15:26:40 +0200

    adding prettier to cloud functions

--- functions/src/storage/optimizeImages.ts
+++ functions/src/storage/optimizeImages.ts
@@ -1,6 +1,4 @@
-import type {
-  StorageObjectData,
-} from "@google/events/cloud/storage/v1/StorageObjectData";
+import type {StorageObjectData} from "@google/events/cloud/storage/v1/StorageObjectData";
 import {storage} from "../helpers/gCloudClients";
 import * as sharp from "sharp";
 
@@ -8,21 +6,16 @@ const options = {
   destBucket: "derlev.xyz",
 };
 
-export const optimizeImages = async (
-  event: StorageObjectData
-) => {
-  if (
-    !event.bucket ||
-    !event.name ||
-    !event.contentType?.startsWith("image/")
-  ) return;
+export const optimizeImages = async (event: StorageObjectData) => {
+  if (!event.bucket || !event.name || !event.contentType?.startsWith("image/"))
+    return;
 
-  const file = storage.bucket(event.bucket)
-    .file(event.name);
+  const file = storage.bucket(event.bucket).file(event.name);
 
   const fileRead = file.createReadStream();
 
-  const sharpRezise = sharp().webp({force: false, quality: 80, effort: 6})
+  const sharpRezise = sharp()
+    .webp({force: false, quality: 80, effort: 6})
     .png({force: false, compressionLevel: 7, effort: 7})
     .jpeg({force: false, quality: 80, mozjpeg: true})
     .gif({force: false})
@@ -30,7 +23,8 @@ export const optimizeImages = async (
 
   const sharpStream = fileRead.pipe(sharpRezise);
 
-  const destFile = storage.bucket(options.destBucket)
+  const destFile = storage
+    .bucket(options.destBucket)
     .file(event.name)
     .createWriteStream();
 
