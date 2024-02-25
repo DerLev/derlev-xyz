c7abcf6dbdd6058ac7bb5f80aa2e6b2aded338e5
DerLev <levin.schroeren@gmail.com>
2024-02-25 18:11:11 +0100

    adding nav & footer + minor adjustments

--- homepage/app/main.ts
+++ homepage/app/main.ts
@@ -3,3 +3,28 @@ import '../styles/app.scss'
 import flamethrower from 'flamethrower-router'
 
 export const router = flamethrower({ prefetch: 'hover', log: false })
+
+/* Intersection Observer for popping out navbar when scrolled */
+const observerCallback: IntersectionObserverCallback = (entries) => {
+  const visible = entries[0].isIntersecting
+  const navBar = document.querySelector("nav")
+  if(!navBar) return
+
+  if(visible) navBar.classList.remove("scrolled")
+  else navBar.classList.add("scrolled")
+}
+
+const observer = new IntersectionObserver(observerCallback, {
+  root: document.body,
+  rootMargin: '0px',
+  threshold: 1,
+})
+
+/* Add observer when HTML has loaded */
+window.addEventListener("load", () => {
+  const contentBegin = document.querySelector("div#content-begin")
+
+  if(contentBegin) {
+    observer.observe(contentBegin)
+  }
+})
