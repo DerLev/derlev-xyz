b51cbb5940217a02a4899969fb71c8d6faa14bf9
DerLev <levin.schroeren@gmail.com>
2024-04-17 23:44:25 +0200

    still fixing nextjs <-> vite collisions + fixing linting errors

--- homepage/app/components/CopyCode.tsx
+++ homepage/app/components/CopyCode.tsx
@@ -1,11 +1,11 @@
-import r2wc from "@r2wc/react-to-web-component"
-import { useState } from "react"
+import r2wc from '@r2wc/react-to-web-component'
+import { useState } from 'react'
 
 const CopyCode = ({ code }: { code: string }) => {
   const [copied, setCopied] = useState(false)
 
   const copyToClipboard = () => {
-    if(copied) return
+    if (copied) return
 
     setCopied(true)
     navigator.clipboard.writeText(code)
@@ -15,21 +15,47 @@ const CopyCode = ({ code }: { code: string }) => {
   }
 
   return (
-    <button className={`copy-code-button ${ copied ? 'copied' : '' }`} onClick={copyToClipboard}>
-      { copied ? (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    <button
+      className={`copy-code-button ${copied ? 'copied' : ''}`}
+      onClick={copyToClipboard}
+    >
+      {copied ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 12.75 6 6 9-13.5"
+          />
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+          />
         </svg>
-      ) }
+      )}
     </button>
   )
 }
 
 export default CopyCode
 
-const WebCopyCode = r2wc(CopyCode, { props: { code: "string" } })
-window.customElements.get("copy-code") || window.customElements.define("copy-code", WebCopyCode)
+const WebCopyCode = r2wc(CopyCode, { props: { code: 'string' } })
+window.customElements.get('copy-code') ||
+  window.customElements.define('copy-code', WebCopyCode)
--- homepage/app/components/NavProgress.tsx
+++ homepage/app/components/NavProgress.tsx
@@ -1,23 +1,23 @@
 import r2wc from '@r2wc/react-to-web-component'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import store from '../store'
 
 const NavProgress = () => {
   const [show, setShow] = useState(true)
 
-  const dispatchCallback = () => {
+  const dispatchCallback = useCallback(() => {
     const currentStore = store.getState()
     const routeLoading = currentStore.routeLoading.value
 
     if (routeLoading !== show) setShow(routeLoading)
-  }
+  }, [show])
 
   useEffect(() => {
     const currentStore = store.getState()
     const routeLoading = currentStore.routeLoading.value
     setShow(routeLoading)
     store.subscribe(dispatchCallback)
-  }, [])
+  }, [dispatchCallback])
 
   return <div className={`nav-loader ${show ? 'active' : ''}`}></div>
 }
